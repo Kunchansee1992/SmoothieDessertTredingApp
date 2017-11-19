3738bff3fe730d0e6478ef40b0a03338a5abf00d
Kunchansee1992 <33746879+Kunchansee1992@users.noreply.github.com>
2017-11-19 07:29:28 +0700

    creat home page

--- app/src/main/java/com/example/ou/smoothiedesserttredingapp/MainActivity.java
+++ app/src/main/java/com/example/ou/smoothiedesserttredingapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.ou.smoothiedesserttredingapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set fonts of slogan
+        txtSlogan = (TextView) findViewById(R.id.txtSlogan);
+        Typeface myCustomFont = Typeface.createFromAsset(getAssets(),"fonts/SHOWG.TTF");
+        txtSlogan.setTypeface(myCustomFont);
     }
 }
